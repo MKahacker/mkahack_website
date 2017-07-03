5740eca30596699c0104662d45d06698a42163a1
Makk-Attack <k-makk@live.com>
2017-07-02 23:59:04 -0500

    Editing Content
    Fixing the content for all the infowindows on the map

--- js/map_api.js
+++ js/map_api.js
@@ -3,14 +3,14 @@ function initMap(){
   var map = new google.maps.Map(document.getElementById('google_api'), {zoom: 10, center: chicago});
    
   var restaurants = [
-        ['Tahoora Sweet', 41.997477, -87.689227,'<h1 style="font-size: 22px">Tahoora Sweets</h1><p style="font-size: 15px">The best bakery for Indian sweets.<span style="display: block font-size: 13px">Recommendation: Gullab Gam from this place</span></p>'],
-        ['Serena Restaurant', 41.997497,-87.687946,'<h1 style="font-size: 22px">Serena Restaurant</h1><p style="font-size: 15px">One of my favorite Indian/Paki restaurant.<br>Recommendation: Haleem (a dish of lentils and beef)</p>'],
-        ['Taqueria San Jose', 41.834915, -87.645925,'<h1>Tahoora Sweets</h1><p>The best bakery for Indian sweets. I would recommend: Gullab Gam from this place</p>'],
-        ['Damascus Falafel', 41.722937, -87.944835,'<h1>Tahoora Sweets</h1><p>The best bakery for Indian sweets. I would recommend: Gullab Gam from this place</p>'],
-        ['Lahore Food & Grill', 41.998018, -87.683277,'<h1>Tahoora Sweets</h1><p>The best bakery for Indian sweets. I would recommend: Gullab Gam from this place</p>'],
-        ['Oberoi\'s Indian Food', 41.838076, -87.633827,'<h1>Tahoora Sweets</h1><p>The best bakery for Indian sweets. I would recommend: Gullab Gam from this place</p>'],
-        ['Vivere',41.880585, -87.630197,'<h1>Tahoora Sweets</h1><p>The best bakery for Indian sweets. I would recommend: Gullab Gam from this place</p>'],
-        ['Magnolia Cafe', 41.883962, -87.628129,'<h1>Hi</h1><p>The best bakery for Indian sweets. I would recommend: Gullab Gam from this place</p>']]; 
+        ['Tahoora Sweet', 41.997477, -87.689227,'<h1 style="font-size: 22px">Tahoora Sweets</h1><p style="font-size: 15px">The best bakery for Indian sweets. Recommendation: Gullab Gam from this place</p>'],
+        ['Serena Restaurant', 41.997497,-87.687946,'<h1 style="font-size: 22px">Serena Restaurant</h1><p style="font-size: 15px">One of my favorite Indian/Paki restaurant.<br> Recommendation: Haleem (a dish of lentils and beef)</p>'],
+        ['Taqueria San Jose', 41.834915, -87.645925,'<h1 style="font-size: 22px">Taqueria San Jose</h1><p style="font-size: 15px">A Taqueria in Bridge Port. I am in love with their tacos.<br> Recommendation: Steak Taco with cilantro and onions.</p>'],
+        ['Damascus Falafel', 41.722937, -87.944835,'<h1 style="font-size: 22px">Damascus Falafel</h1><p style="font-size: 15px">The best Mediterranean food around the Chicago area "Mediterranean Chipotle"<br> Recommendation: Damascus way shawarma roll</p>'],
+        ['Lahore Food & Grill', 41.998018, -87.683277,'<h1 style="font-size: 22px">Lahore Food & Grill</h1><p style="font-size: 15px">An restaurant that serves Paki food<br> Recommendation: Chicken Chargha (Might Be Too Spicy)</p>'],
+        ['Oberoi\'s Indian Food', 41.838076, -87.633827,'<h1 style="font-size: 22px">Damascus Falafel</h1><p style="font-size: 15px">The best Mediterranean food around the Chicago area "Mediterranean Chipotle"<br>Recommendation: Damascus way shawarma roll</p>'],
+        ['Vivere',41.880585, -87.630197,'<h1 style="font-size: 22px">Damascus Falafel</h1><p style="font-size: 15px">The best Mediterranean food around the Chicago area "Mediterranean Chipotle"<br>Recommendation: Damascus way shawarma roll</p>'],
+        ['Magnolia Cafe', 41.883962, -87.628129,'<h1 style="font-size: 22px">Damascus Falafel</h1><p style="font-size: 15px">The best Mediterranean food around the Chicago area "Mediterranean Chipotle"<br>Recommendation: Damascus way shawarma roll</p>']]; 
   
   var fork ={ url:'images/fork2.png'};
   
