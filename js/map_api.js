3b9877acec58527292048169086c114c394a5195
Makk-Attack <k-makk@live.com>
2017-07-03 22:11:23 -0500

    Info Window
    Applying the stying to the rest of the info windows

--- js/map_api.js
+++ js/map_api.js
@@ -4,13 +4,13 @@ function initMap(){
    
   var restaurants = [
         ['Tahoora Sweet', 41.997477, -87.689227,'<h1 style="font-size: 22px">Tahoora Sweets</h1><p style="font-size: 15px">The best bakery for Indian sweets.</p><p style="font-size: 13px; font-weight: 700">Recommendation: Gullab Gam from this place</p>'],
-        ['Serena Restaurant', 41.997497,-87.687946,'<h1 style="font-size: 22px">Serena Restaurant</h1><p style="font-size: 15px">One of my favorite Indian/Paki restaurant.<br> Recommendation: Haleem (a dish of lentils and beef)</p>'],
-        ['Taqueria San Jose', 41.834915, -87.645925,'<h1 style="font-size: 22px">Taqueria San Jose</h1><p style="font-size: 15px">A Taqueria in Bridge Port. I am in love with their tacos.<br> Recommendation: Steak Taco with cilantro and onions.</p>'],
-        ['Damascus Falafel', 41.722937, -87.944835,'<h1 style="font-size: 22px">Damascus Falafel</h1><p style="font-size: 15px">The best Mediterranean food around the Chicago area "Mediterranean Chipotle"<br> Recommendation: Damascus way shawarma roll</p>'],
-        ['Lahore Food & Grill', 41.998018, -87.683277,'<h1 style="font-size: 22px">Lahore Food & Grill</h1><p style="font-size: 15px">An restaurant that serves Paki food<br> Recommendation: Chicken Chargha (Might Be Too Spicy)</p>'],
-        ['Oberoi\'s Indian Food', 41.838076, -87.633827,'<h1 style="font-size: 22px">Oberoi\'s Indian Food</h1><p style="font-size: 15px">A very conveniently located Indian restaurant around the IIT campus<br> Recommendation: Mango Lassi</p>'],
-        ['Vivere',41.880585, -87.630197,'<h1 style="font-size: 22px">Vivere</h1><p style="font-size: 15px">A very fancy Italian restaurant downtown.<br> Recommendation: Ravioli di Formaggi con Pomodoro</p>'],
-        ['Magnolia Cafe', 41.883962, -87.628129,'<h1 style="font-size: 22px">Magnolia Cafe</h1><p style="font-size: 15px">A popular coffee shop and bakery. Located on heart of Downtown<br> Recommendation: Caramel Latte</p>']]; 
+        ['Serena Restaurant', 41.997497,-87.687946,'<h1 style="font-size: 22px">Serena Restaurant</h1><p style="font-size: 15px">One of my favorite Indian/Paki restaurant.</p><p style="font-size: 13px; font-weight: 700">Recommendation: Haleem (a dish of lentils and beef)</p>'],
+        ['Taqueria San Jose', 41.834915, -87.645925,'<h1 style="font-size: 22px">Taqueria San Jose</h1><p style="font-size: 15px">A Taqueria in Bridge Port. I am in love with their tacos.</p><p style="font-size: 13px; font-weight: 700">Recommendation: Steak Taco with cilantro and onions.</p>'],
+        ['Damascus Falafel', 41.722937, -87.944835,'<h1 style="font-size: 22px">Damascus Falafel</h1><p style="font-size: 15px">The best Mediterranean food around the Chicago area "Mediterranean Chipotle"</p><p style="font-size: 13px; font-weight: 700">Recommendation: Damascus way shawarma roll</p>'],
+        ['Lahore Food & Grill', 41.998018, -87.683277,'<h1 style="font-size: 22px">Lahore Food & Grill</h1><p style="font-size: 15px">An restaurant that serves Paki food</p><p style="font-size: 13px; font-weight: 700">Recommendation: Chicken Chargha (Might Be Too Spicy)</p>'],
+        ['Oberoi\'s Indian Food', 41.838076, -87.633827,'<h1 style="font-size: 22px">Oberoi\'s Indian Food</h1><p style="font-size: 15px">A very conveniently located Indian restaurant around the IIT campus</p><p style="font-size: 13px; font-weight: 700">Recommendation: Mango Lassi</p>'],
+        ['Vivere',41.880585, -87.630197,'<h1 style="font-size: 22px">Vivere</h1><p style="font-size: 15px">A very fancy Italian restaurant downtown.</p><p style="font-size: 13px; font-weight: 700">Recommendation: Ravioli di Formaggi con Pomodoro</p>'],
+        ['Magnolia Cafe', 41.883962, -87.628129,'<h1 style="font-size: 22px">Magnolia Cafe</h1><p style="font-size: 15px">A popular coffee shop and bakery. Located on heart of Downtown</p><p style="font-size: 13px; font-weight: 700">Recommendation: Caramel Latte</p>']]; 
   
   var fork ={ url:'images/fork2.png'};
   
